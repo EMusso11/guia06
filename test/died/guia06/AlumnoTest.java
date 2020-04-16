e585d3a345d08ff0b4542b40b433efa726d6622c
Emilio Musso <56137129+EMusso11@users.noreply.github.com>
2020-04-16 15:58:47 -0300

    Add files via upload

--- test/died/guia06/AlumnoTest.java
+++ test/died/guia06/AlumnoTest.java
@@ -1,24 +1,69 @@
 package died.guia06;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 class AlumnoTest {
 
 	@Test
 	void testCreditosObtenidos() {
-		fail("Not yet implemented");
+		List<Curso> listCursos = new ArrayList<Curso>();
+		Curso c1 = new Curso(30);
+		listCursos.add(c1);
+		Curso c2 = new Curso(20);
+		listCursos.add(c2);
+		Alumno unAlum = new Alumno();
+		unAlum.setAprobados(listCursos);
+		
+		assertSame(50, unAlum.creditosObtenidos());
 	}
-
+/*
 	@Test
-	void testAprobar() {
-		fail("Not yet implemented");
+	void testAprobarEliminaCursada() {
+		List<Curso> list1 = new ArrayList<Curso>();
+		List<Curso> list2 = new ArrayList<Curso>();
+		Alumno unAlum = new Alumno();
+		
+		Curso unCurso = new Curso();
+		unCurso.setId(1234);
+		list1.add(unCurso);
+		unAlum.setCursando(list1);				// estaba cursando un curso
+		
+		unAlum.aprobar(unCurso);				// lo aprueba
+		
+		assertSame(unAlum.getCursando(), list2);// deberia quedar sin cursos en cursando
 	}
-
+	
 	@Test
-	void testInscripcionAceptada() {
-		fail("Not yet implemented");
+	void testAprobarAgregaAprobada() {
+		List<Curso> list1 = new ArrayList<Curso>();
+		List<Curso> list2 = new ArrayList<Curso>();
+		Alumno unAlum = new Alumno();
+		
+		Curso unCurso = new Curso();
+		unCurso.setId(1234);
+		list2.add(unCurso);
+		unAlum.setCursando(list1);
+		
+		unAlum.aprobar(unCurso);
+		
+		assertSame(unAlum.getAprobados(), list2);
 	}
-
+	@Test
+	void testInsripcionAceptada() {
+		List<Curso> list1 = new ArrayList<Curso>();
+		List<Curso> list2 = new ArrayList<Curso>();
+		Alumno unAlum = new Alumno();
+		
+		Curso unCurso = new Curso();
+		unCurso.setId(1234);
+		list2.add(unCurso);
+		unAlum.setCursando(list1);
+		
+		unAlum.inscripcionAceptada(unCurso);
+		
+		assertSame(unAlum.getCursando(), list2);
+	}*/
 }
+
